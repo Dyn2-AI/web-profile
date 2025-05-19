3b780a62f6532d10036fafb4751a3b237664b19a
Asep Solahudin <solahudinasep12@gmail.com>
2025-05-19 14:18:22 +0700

    profil baru dibuat

--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,30 @@
-export default function Home() {
-  return <main>Homepage</main>;
-}
+import { Button } from "@/components/ui/button";
+import { FiDownload } from "react-icons/fi";
+
+const Home = () => {
+  return (
+    <section className="h-full">
+      <div className="container mx-auto h-full">
+        <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
+          {/* text bio */}
+          <div className="text-center xl:text-left">
+            <span className="text-xl">Software Developer</span>
+            <h1 className="h1 mb-6">
+              Hello I'm <br />{" "}
+              <span className="text-accent">Asep Solahudin</span>
+            </h1>
+            <p>
+              I excel in web development, have strong skills in several
+              programming languages, and a deep interest in security and
+              artificial intelligence. I'm eager to keep learning and growing in
+              the evolving world of technology.
+            </p>
+          </div>
+          <div>photo</div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Home;
